6feff1027121153dd74bd427ca0e749030fa2218
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-11-08 21:47:42 +0100

    dokcer image + circle ci pipeline

---
+++ asterisk/container/etc.asterisk.rtp.conf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p /etc/asterisk
+cat etc.asterisk.rtp.conf /etc/asterisk/rtp.conf
+sed -i "s/rtpstart=10000/rtpstart=16384/g" /etc/asterisk/rtp.conf
+sed -i "s/rtpend=20000/rtpend=16394/g" /etc/asterisk/rtp.conf
